d4e3c21e9925c2c12b05329e26ae9071f19e14d6
Nicola Pasquali <nicola_pasquali@outlook.it>
2018-08-29 20:50:13 +0200

    Created new branch to test RPG variant of our idea. General refactor and cleaning of index.js

--- src/index.js
+++ src/index.js
@@ -1,101 +1,32 @@
 import '../lib/kontra'; // FIXME Sostituire con la versione minificata
-import Player from './models/Player';
-import {createEnemies, createSpriteFromEntity} from './utilities';
+import {createBackground} from './utilities';
 
 kontra.init('gameScreen');
 
-let connected = checkOnlineStatus();
-const player = new Player(520, 20);
-player.sprite = createSpriteFromEntity(kontra, player);
-const background = kontra.sprite({
-    x: 0,
-    y: 0,
-    color: 'black',
-    width: kontra.canvas.width,
-    height: kontra.canvas.height
-});
-let enemies = new Set(createEnemies(kontra, 2));
+let connected;
+let background;
 
-function init() {
-    window.addEventListener('load', function (e) {
-        connected = navigator.onLine;
-    }, false);
-    console.log('init');
+function initialize() {
+    setConnectionStatusListeners();
+    background = createBackground(kontra, 'black');
+    kontra.gameLoop({
+        fps: 60,
+        update: gameLoopUpdate,
+        render: gameLoopRender
+    }).start();
+}
+
+function setConnectionStatusListeners() {
+    window.addEventListener('load', (e) => connected = navigator.onLine, false);
+    window.addEventListener('online', (e) => connected = true, false);
+    window.addEventListener('offline', (e) => connected = false, false);
 }
 
 function gameLoopUpdate() {
-    setPlayerControls();
-    setPlayerScreenBoundaries();
-    // enemies.forEach((enemy) => enemy.patrol());
-}
-
-function setPlayerControls() {
-    if (kontra.keys.pressed('left') || kontra.keys.pressed('a')) {
-        player.sprite.dx = -player.speed;
-    } else if (kontra.keys.pressed('right') || kontra.keys.pressed('d')) {
-        player.sprite.dx = player.speed;
-    } else {
-        player.sprite.dx = 0;
-    }
-
-    if (kontra.keys.pressed('up') || kontra.keys.pressed('w')) {
-        player.sprite.dy = -player.speed;
-    } else if (kontra.keys.pressed('down') || kontra.keys.pressed('s')) {
-        player.sprite.dy = player.speed;
-    } else {
-        player.sprite.dy = 0;
-    }
-}
-
-function setPlayerScreenBoundaries() {
-    if (player.sprite.x + player.sprite.width > kontra.canvas.width) {
-        player.sprite.x = kontra.canvas.width - player.sprite.width;
-    }
-    if (player.sprite.x < 0) {
-        player.sprite.x = 0;
-    }
-    if (player.sprite.y + player.sprite.height > kontra.canvas.height) {
-        player.sprite.y = kontra.canvas.height - player.sprite.height;
-    }
-    if (player.sprite.y < 0) {
-        player.sprite.y = 0;
-    }
 }
 
 function gameLoopRender() {
     background.render();
-    checkCollisions();
-    player.sprite.render();
-    if (connected) {
-        enemies.forEach((guard) => guard.sprite.render());
-    } else {
-        console.log('NOT connected');
-    }
 }
 
-function checkCollisions() {
-    enemies.forEach((guard) => {
-        if (guard.sprite.collidesWith(player.sprite)) {
-            // player = undefined;
-        }
-    });
-}
-
-function checkOnlineStatus() {
-    window.addEventListener('online', function (e) {
-        connected = true;
-    }, false);
-
-    window.addEventListener('offline', function (e) {
-        connected = false;
-    }, false);
-}
-
-const loop = kontra.gameLoop({
-    fps: 60,
-    update: gameLoopUpdate,
-    render: gameLoopRender
-});
-
-init();
-loop.start();
+initialize();
--- src/utilities.js
+++ src/utilities.js
@@ -20,3 +20,13 @@ export function createEnemies(kontra, numberOfGuards) {
     }
     return enemies;
 }
+
+export function createBackground(kontra, color) {
+    return kontra.sprite({
+        x: 0,
+        y: 0,
+        color: color,
+        width: kontra.canvas.width,
+        height: kontra.canvas.height
+    });
+}
