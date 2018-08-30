ef47ac4f58f548cd3140e31593a69362bc052a90
Veronika <vero.milic@gmail.com>
2018-08-30 23:02:22 +0200

    copiato da doc ufficiale, ma kontra.spriteSheet is not a function

--- src/index.js
+++ src/index.js
@@ -25,13 +25,60 @@ function setConnectionStatusListeners() {
     window.addEventListener('offline', (e) => connected = false, false);
 }
 
+
+
+let image = new Image();
+image.src = 'assets/sprite.png';
+
+let spriteSheet = kontra.spriteSheet({
+    image: image,
+    frameWidth: 16,
+    frameHeight: 16,
+
+    // this will also call createAnimations()
+    animations: {
+      // create 1 animation: idle
+      idle: {
+        frames: 1  // single frame - email
+        }
+    }
+});
+
+spriteSheet.createAnimations({
+    man1: {
+      frames: [5, 6],
+      frameRate: 10,
+      loop: true
+  },
+  man2: {
+      frames: [9,10],
+      frameRate: 10,
+      loop: true
+  },
+  man2: {
+      frames: [13,14],
+      frameRate: 10,
+      loop: true
+  },
+  bug: {
+      frames: [7, 11], 
+      frameRate: 10,
+      loop: true
+  }
+});
+
 function gameLoopUpdate() {
     menu.manageInput();
+    spriteSheet.animations.man1.update();
 }
 
 function gameLoopRender() {
     background.render();
     menu.render();
+    spriteSheet.animations.man1.render({x: 10, y: 10,});
 }
 
-initialize();
+image.onload = function() {
+  initialize();
+};
+
