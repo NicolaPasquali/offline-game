4e7ea2d4f50d96e8bb942f310c86bded170467f1
Nicola Pasquali <nicola_pasquali@outlook.it>
2018-09-03 22:10:34 +0200

    Minor changes to index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import '../lib/kontra'; // FIXME Sostituire con la versione minificata
 import {createBackground} from './utilities';
-import InformationDisplay from "./models/InformationDisplay";
+import InformationDisplay from './models/InformationDisplay';
 import Player from './models/Player';
 import BasicEnemy from "./models/enemies/BasicEnemy";
 
@@ -20,11 +20,8 @@ function initialize() {
     initializeInformationDisplay();
     informationDisplay.render();
 
-    enemies[0].hp = 10;
-    informationDisplay.render();
-
     kontra.gameLoop({
-        fps: 5,
+        fps: 30,
         update: gameLoopUpdate,
         render: gameLoopRender
     }).start();
