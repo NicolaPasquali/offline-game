e63d57941d0d8c4fc0faf5dbab3aa85064cbd60f
Nicola Pasquali <nicola_pasquali@outlook.it>
2018-09-11 22:05:19 +0200

    Updated Webpack dev configuration in order to move the images into dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -45,14 +45,14 @@ module.exports = {
             filename: path.join(__dirname, 'dist', 'index.html'),
             template: path.join(__dirname, 'src', 'index.html'),
             minify: false
-        })
-        // new ImageminPlugin({
-        //     externalImages: {
-        //         context: 'src/assets',
-        //         sources: glob.sync('src/assets/*.png'),
-        //         destination: 'dist/assets'
-        //     }
-        // }),
+        }),
+        new ImageminPlugin({
+            externalImages: {
+                context: 'src/assets',
+                sources: glob.sync('src/assets/*.png'),
+                destination: 'dist/assets'
+            }
+        }),
         // new BundleAnalyzerPlugin() // Utile per vedere cosa si può fare per snellire il bundle
     ]
 };
