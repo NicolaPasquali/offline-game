3d1c473f0a81d38f5ac39b98ac9ce8c205cebe24
Veronika <vero.milic@gmail.com>
2018-09-12 19:23:54 +0200

    online block

--- src/index.js
+++ src/index.js
@@ -9,9 +9,18 @@ function initialize() {
 }
 
 function setConnectionStatusListeners() {
-    window.addEventListener('load', (e) => connected = navigator.onLine, false);
-    window.addEventListener('online', (e) => connected = true, false);
-    window.addEventListener('offline', (e) => connected = false, false);
+    window.addEventListener('load', (e) => toggleVisibility(navigator.onLine), false);
+    window.addEventListener('online', (e) => toggleVisibility(true), false);
+    window.addEventListener('offline', (e) => toggleVisibility(false), false);
+}
+
+function toggleVisibility(connected) {
+	//TODO: togliere commento in production!!!
+	// if (connected) {
+	// 	document.getElementById('online-block').style.display = 'block';
+	// } else {
+ //    	document.getElementById('online-block').style.display = 'none';
+	// }
 }
 
 initialize();
